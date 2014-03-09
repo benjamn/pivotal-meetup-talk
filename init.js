06ad34d0fff0cef9bc9152aa8b8a8ab438eff749
Ben Newman <bn@cs.stanford.edu>
2014-03-09 16:10:28 -0700

    Spaces instead of tabs in init.js.

--- init.js
+++ init.js
@@ -1,20 +1,19 @@
 // Full list of configuration options available here:
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
-	controls: true,
-	progress: true,
-	history: true,
-	center: true,
+    width: 1280,
+    height: 720,
 
-	theme: "simple",
-	transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
 
-	// Parallax scrolling
-	// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
-	// parallaxBackgroundSize: '2100px 900px',
+    theme: "simple",
+    transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
 
-	// Optional libraries used to extend on reveal.js
-	dependencies: [{
+    // Optional libraries used to extend on reveal.js
+    dependencies: [{
         src: 'lib/js/classList.js',
         condition: function() {
             return !document.body.classList;
@@ -32,7 +31,7 @@ Reveal.initialize({
     }, {
         src: 'plugin/highlight/highlight.js',
         async: true,
-		callback: function() {
+        callback: function() {
             Array.prototype.map.call(
                 document.querySelectorAll("pre code"),
                 function(code) {
